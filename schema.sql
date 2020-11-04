c4b9ca0e958219de9879cce29abc54398cbb4338
Matt Boggs <matt.david.boggs@gmail.com>
2020-11-04 13:46:15 -0700

    env is working

--- schema.sql
+++ schema.sql
@@ -1,4 +1,6 @@
 -- Drops the blogger if it exists currently --
-DROP DATABASE IF EXISTS blogger;
+DROP DATABASE IF EXISTS bookclub_DB;
 -- Creates the "blogger" database --
-CREATE DATABASE blogger;
+CREATE DATABASE bookclub_DB;
+
+USE bookclub_DB;
